e1e1fef7c1b640543d03a5ce8ad432f51a03d98d
Elite Robust Ontology <erandix9@gmail.com>
2025-12-12 15:53:39 +0300

    Updates

--- Library/Services/Eliton.js
+++ Library/Services/Eliton.js
@@ -20,7 +20,7 @@ const AUTORUN=()=>{
    
     },()=>{
 
-        import("./Connection/Connection.js").then(o=>typeof o.CONNECTIONS=="function"&&o.CONNECTIONS()).catch(console.error);
+        import("https://erandroid9.github.io/Eliton/Connection/Connection.js").then(o=>typeof o.CONNECTIONS=="function"&&o.CONNECTIONS()).catch(console.error);
    
     });
 
