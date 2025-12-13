91c5a9776196d81ffd0c89c4776e3179c32d29cc
Elite Robust Ontology <erandix9@gmail.com>
2025-12-13 09:36:12 +0300

    Updates

--- Library/Services/Constants.js
+++ Library/Services/Constants.js
@@ -1,5 +1,7 @@
-const CLOUDCONNECTION="";
-const CLOUDDATABASECREATION="";
-const CLOUDTABLECREATION="";
-const CLOUDDATAINSERTATION="";
-const CLOUDDATAUPDATION="";
+const CLOUDCONNECTION="https://script.google.com/macros/s/AKfycbyJentJPp84534OjvsE80-H9WQM87F6ibh2SleHQjNtO6Cu1YIK4N-q3WTc6FuU9CowsA/exec";
+const CLOUDDATABASECREATION="https://script.google.com/macros/s/AKfycby9J7VFeCCGLlQGSJ9UU2hTvpQidSGlk2-cW72Ze2JeJhu6vKLK--UPSN7NferexmIZCg/exec";
+const CLOUDTABLECREATION="https://script.google.com/macros/s/AKfycbxurjKSdE49CqODwib_fNdnXlqITknBwGtL4fg6iYylz2MUMdbNvIEkeHz3YytUN-heHw/exec";
+const CLOUDDATAINSERTATION="https://script.google.com/macros/s/AKfycbw4KJHKe7zwlBuqcGD26Tv2UhTYiUtNspSehlGHwPoU8o5sAEcPwBI4iKwcfHMnJL9sbw/exec";
+const CLOUDDATAUPDATION="https://script.google.com/macros/s/AKfycby8A1GVoD8G1JtLmiSHhQ54XjwM_2COloZAoAoSjr50hdNBLkmuoh7NFReEoP-hJflKOg/exec";
+const CLOUDDATAGETTING="https://script.google.com/macros/s/AKfycbxi4dQQfm5u5Pd4Id3JCpmBbRhXv_y4XZZnwET4MHYoWfqKaXhqMXtc2T1gklUuocVy3A/exec";
+const CLOUDEMAILDATA="https://script.google.com/macros/s/AKfycbw4hg4km0kWeCzPezNwxtY5miuG05JS_YGy8GMqTXjSB06v69wCpeVkH049Hidkp0hxVw/exec";
