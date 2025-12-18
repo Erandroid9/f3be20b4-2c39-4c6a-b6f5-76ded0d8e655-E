e20685b32f23d9dc4e643e03b2365f0374447b25
Elite Robust Ontology <erandix9@gmail.com>
2025-12-18 19:32:58 +0300

    Updates

--- Library/Pages/DataBase/DataLoader/DataLoader.js
+++ Library/Pages/DataBase/DataLoader/DataLoader.js
@@ -68,7 +68,7 @@ export const DATALOADER=()=>{
 
                                         CONFIGENVIRONMENT();
 
-                                        //RERUN();
+                                        RERUN();
                                                 
                                     };
                                     
