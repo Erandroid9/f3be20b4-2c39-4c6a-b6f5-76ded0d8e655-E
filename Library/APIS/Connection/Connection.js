78257cfd4132514dae3aea3a499384ceb64ec695
Elite Robust Ontology <erandix9@gmail.com>
2025-12-28 09:02:46 +0300

    Updates

--- Library/APIS/Connection/Connection.js
+++ Library/APIS/Connection/Connection.js
@@ -1,6 +1,7 @@
 import { GETCOUNTRIESAPI } from "../DataBase/Countries/Countries.js";
 import { GETMOVIESAPI } from "../DataBase/Movies/Movies.js";
 import { GETMUSICAPI } from "../DataBase/Music/GetMusic.js";
+import { GETPOLICIESAPI } from "../DataBase/Policies/Policies.js";
 import { GETUGANDANDISTRICTSAPI } from "../DataBase/UgandanDistricits/UgandanDistricits.js";
 
 export const APICONNECTION=()=>{
@@ -8,9 +9,10 @@ const DATA=`const GETMUSICAPI=${GETMUSICAPI};
 const GETMOVIESAPI=${GETMOVIESAPI};
 const GETCOUNTRIESAPI=${GETCOUNTRIESAPI};
 const GETUGANDANDISTRICTSAPI=${GETUGANDANDISTRICTSAPI};
+const GETPOLICIESAPI=${GETPOLICIESAPI};
 `;
 localStorage.setItem("APIS",DATA);
 };
 `
-const GETUGANDANDISTRICTSAPI=${GETUGANDANDISTRICTSAPI};
+const GETPOLICIESAPI=${GETPOLICIESAPI};
 `
