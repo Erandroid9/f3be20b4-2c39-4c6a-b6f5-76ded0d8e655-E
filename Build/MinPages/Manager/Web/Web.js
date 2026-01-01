d1b73c008137a44a58b10707c8799731763aeac5
Elite Robust Ontology <erandix9@gmail.com>
2026-01-01 08:43:56 +0300

    Updates

--- Build/MinPages/Manager/Web/Web.js
+++ Build/MinPages/Manager/Web/Web.js
@@ -104,15 +104,7 @@ const MOBILEVIEW=()=>{
 
             CLICK(ELEMENTS,()=>{
 
-                const HEADERS=["Name","Android","Desktop","Web","Date"];
-
-                const INFO=[sessionStorage.getItem("Name"),sessionStorage.getItem("Android"),sessionStorage.getItem("Desktop"),sessionStorage.getItem("Web"),new Date()];
-                    
-                INSERTDATA(ERANDIXMANAGERLINK,"Production",HEADERS,INFO,(DataS)=>{
-                    
-                    TOASTVIEW("Data Posted To Server");
-                    
-                });
+                ERANDIXNEWPROJECT();
 
             });
 
