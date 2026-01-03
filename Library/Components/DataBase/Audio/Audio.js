5dce0f47dcadb7d240f7d2c0ab894d87e51b6796
Elite Robust Ontology <erandix9@gmail.com>
2026-01-03 08:07:52 +0300

    Updates

--- Library/Components/DataBase/Audio/Audio.js
+++ Library/Components/DataBase/Audio/Audio.js
@@ -2,8 +2,9 @@ export const AUDIO = (HOLDER, SRC, callBack) => {
 
     ELEMENTED(HOLDER, "audio", (ELEMENT) => {
 
-        SOURCED(ELEMENT, SRC);
-        CONTROLS(ELEMENT);
+        ELEMENT.dataset.src = SRC;
+        ELEMENT.preload = "none";
+        ELEMENT.controls = true;
 
         POSITIONRELATIVE(ELEMENT);
         WIDTH(ELEMENT, "");
@@ -12,22 +13,60 @@ export const AUDIO = (HOLDER, SRC, callBack) => {
         DISPLAYBLOCK(ELEMENT);
         BACKGROUND(ELEMENT, WHITE);
 
+        const loadAudio = () => {
+
+            CHECKER(!ELEMENT.src,()=>{
+
+                ELEMENT.src = ELEMENT.dataset.src;
+
+            });
+
+        };
+
+        CONDITION("IntersectionObserver" in window,()=>{
+
+            const observer = new IntersectionObserver(entries => {
+
+                entries.forEach(entry => {
+
+                    CHECKER(entry.isIntersecting,()=>{
+
+                        loadAudio();
+
+                        observer.disconnect();
+
+                    });
+
+                });
+
+            });
+
+            observer.observe(ELEMENT);
+
+        },()=>{
+
+            loadAudio();
+
+        });
+
         ELEMENT.addEventListener("play", () => {
+
+            loadAudio();
 
             document.querySelectorAll("audio").forEach(audio => {
 
-                CHECKER(audio !== ELEMENT,()=>{
+                CHECKER(audio !== ELEMENT, () => {
 
                     audio.pause();
 
                 });
-            
+
             });
 
         });
 
         callBack(ELEMENT);
-
+        
     });
 
 };
