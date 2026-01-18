edb00adcbf47ee036a357692df5554f806e79900
Elite Robust Ontology <erandix9@gmail.com>
2026-01-19 01:25:45 +0300

    Updates

--- Library/PlugIns/Connection/Connections.js
+++ Library/PlugIns/Connection/Connections.js
@@ -1,8 +1,10 @@
 import { USSD } from "../DataBase/RunUssd/RunUssd.js";
+import { onUSSDResponse } from "../DataBase/UssdResponse/UssdResponse.js";
 
 export const PLUGINCONNECTIONS=()=>{
 
 const DATA=`const USSD=${USSD};
+const onUSSDResponse=${onUSSDResponse};
 `;
 
 localStorage.setItem("PLUGINS",DATA);
@@ -10,5 +12,5 @@ localStorage.setItem("PLUGINS",DATA);
 };
 
 `
-const USSD=${USSD};
+const onUSSDResponse=${onUSSDResponse};
 `
---
+++ Library/PlugIns/DataBase/UssdResponse/UssdResponse.js
@@ -0,0 +1,5 @@
+export const onUSSDResponse=(response,callback)=>{
+
+    callback(response);
+
+};
