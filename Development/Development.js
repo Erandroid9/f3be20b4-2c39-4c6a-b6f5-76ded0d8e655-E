3010de020d2c70fb73206e96ee2a404d28b07bdf
Elite Robust Ontology <erandix9@gmail.com>
2026-01-26 18:54:46 +0300

    Updates

--- Development/Development.js
+++ Development/Development.js
@@ -27,39 +27,11 @@ const ERANDES=()=>{
 
 const PAGESMANAGERS=()=>{
 
-    SWITCHER("800px",()=>{DESKTOPVIEW()},()=>{MOBILEVIEW();})
+    SWITCHER("800px",()=>{HOMEPAGE()},()=>{HOMEPAGE();})
 
 };
 
-const DESKTOPVIEW=()=>{
-    
-    HEADERFOOTERVIEW((ELEMENT)=>{
-
-        LEFTTEXTVIEW(ELEMENT,"Qel Medistore",()=>{
-
-        });
-
-    },(ELEMENT)=>{
-
-    },(ELEMENT)=>{
-
-        TEXTVIEW(ELEMENT,"Contact Us",()=>{
-            
-        });
-
-        TEXTVIEW(ELEMENT,"App",()=>{
-            
-        });
-
-        TEXTVIEW(ELEMENT,"Policies",()=>{
-            
-        });
-
-    });
-
-};
-
-const MOBILEVIEW=()=>{
+const HOMEPAGE=()=>{
 
     HEADERFOOTERVIEW((ELEMENT)=>{
 
@@ -73,13 +45,112 @@ const MOBILEVIEW=()=>{
 
     },(ELEMENT)=>{
 
+        GETSAVEDINDEX("Catergory",(Data)=>{
+
+            CHECKER(Data.Approved,()=>{
+                
+                DIVVIEW(ELEMENT,"95%","300px",TRANSPARENT,(ELEMENTS0)=>{
+
+                    MARGIN(ELEMENTS0,"2%");
+
+                    OVERFLOW(ELEMENTS0);
+
+                    HEADER(ELEMENTS0,(ELES)=>{
+
+                        LEFTTEXTVIEW(ELES,Data.ProductName,(ELEMENTSA)=>{
+
+                        });
+
+                    });
+
+                    INLINEVIEW(ELEMENTS0,"","auto",TRANSPARENT,"",(ELEMENTA)=>{
+
+                        POSITIONABSOLUTE(ELEMENTA);
+                        BOTTOM(ELEMENTA,"");
+                        TOP(ELEMENTA,"50px");
+                        LEFT(ELEMENTA,"");
+                        OVERFLOWHIDDEN(ELEMENTA);
+                        OVERFLOWX(ELEMENTA);
+
+                        GETSAVEDINDEX("Products",(Datate)=>{
+
+                            CHECKER(Data.ID === Datate.ProductCatergory && Datate.Approved,()=>{
+
+                                INLINEVIEW(ELEMENTA,"150px","100%",TRANSPARENT,"2%",(ELEMENTA)=>{
+
+                                    FLEXSHRINK(ELEMENTA);
+
+                                    IMAGE(ELEMENTA,"","",Datate.ProductImage,TRANSPARENT,(ELEMENTSS)=>{
+
+                                        COVEROBJECTFIT(ELEMENTSS);
+
+                                    });
+
+                                    HEADER(ELEMENTA,(ELES)=>{
+
+                                        RIGHTTEXTVIEW(ELES,Datate.ProductNumber,(ELEMENTSA)=>{
+
+                                            FONTSIZE(ELEMENTSA,"20px");
+
+                                            PADDING(ELEMENTSA,"2%");
+
+                                            BACKGROUND(ELEMENTSA,BLACK);
+
+                                            COLOR(ELEMENTSA,FORESTGREEN);
+
+                                        });
+
+                                    });
+
+                                    FOOTER(ELEMENTA,(ELES)=>{
+
+                                        BACKGROUND(ELES,BLACK);
+
+                                        LEFTTEXTVIEW(ELES,Datate.ProductName,(ELEMENTSA)=>{
+
+                                            FONTSIZE(ELEMENTSA,"15px");
+
+                                        });
+                
+                                    });
+
+                                    CLICK(ELEMENTA,()=>{
+
+                                        JSONIFICATION (Datate,(Dares)=>{
+
+                                            DATASTORE("","Items",Dares);
+
+                                        });
+
+                                    });
+
+                                });
+
+                            });
+
+                        });
+ 
+                    });
+   
+                });
+      
+            });
+
+        });
+
     },(ELEMENT)=>{
 
         TEXTVIEW(ELEMENT,"Contact Us",()=>{
             
         });
 
-        TEXTVIEW(ELEMENT,"App",()=>{
+        TEXTVIEW(ELEMENT,"App",(ELEMENTS)=>{
+
+            CLICK(ELEMENTS,()=>{
+
+                WEBSITE("");
+
+            });
             
         });
 
