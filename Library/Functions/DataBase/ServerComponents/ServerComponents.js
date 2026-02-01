316fbde76542db0c889ec6da8466c49a49b18837
Elite Robust Ontology <erandix9@gmail.com>
2026-02-01 17:38:23 +0300

    Updates

--- Library/Functions/DataBase/ServerComponents/ServerComponents.js
+++ Library/Functions/DataBase/ServerComponents/ServerComponents.js
@@ -2,11 +2,11 @@ export const SERVERCOMPONENTS=(PATH,CALLBACK)=>{
 
     CONDITION(localStorage.getItem("Env") === "Dev",()=>{
 
-        CALLBACK("https://erandroid9.github.io/Eliton/Library/Assets/DataBase/Raw/Images/"+PATH);
+        CALLBACK("https://erandroid9.github.io/Eliton/Library/Assets/DataBase/Raw/"+PATH);
 
     },()=>{
 
-        CALLBACK("https://erandroid9.github.io/Eliton/Library/Assets/DataBase/Raw/Images/"+PATH);
+        CALLBACK("https://erandroid9.github.io/Eliton/Library/Assets/DataBase/Raw/"+PATH);
 
     });
 
