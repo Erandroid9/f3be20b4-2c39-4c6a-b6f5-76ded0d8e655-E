24dd598af2ccc1512bf3390b32402aa5a7d82c8b
Elite Robust Ontology <erandix9@gmail.com>
2026-02-01 12:27:31 +0300

    Updates

--- Connection/Connection.js
+++ Connection/Connection.js
@@ -1,5 +1,4 @@
 import { ASSETSCONNECTION } from "../Library/Assets/Connection/Connection.js";
-import { OFFENVIRONMENT } from "../Library/Environment/DataBase/Off/Off.js";
 import { DATALOADER } from "../Library/Pages/DataBase/DataLoader/DataLoader.js";
 import { DISPLAYLOADER } from "../Library/Pages/DataBase/DisplayLoader/DisplayLoader.js";
 
