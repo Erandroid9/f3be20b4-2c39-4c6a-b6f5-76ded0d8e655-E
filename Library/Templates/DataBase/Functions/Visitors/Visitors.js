cec8b1e007909fcfb6e5fbc37b826bbed772c706
Elite Robust Ontology <erandix9@gmail.com>
2026-02-04 20:59:05 +0300

    Updates

--- Library/Templates/DataBase/Functions/Visitors/Visitors.js
+++ Library/Templates/DataBase/Functions/Visitors/Visitors.js
@@ -1,51 +1,59 @@
 export const  VISITORS=()=>{
 
-    const HEADERS=["DeviceName","DeviceTime","VisitedTimes","Platform","Language","Screen","FullData","Approved"];
+    CONDITION(localStorage.getItem("Env") === "Dev" ,()=>{
 
-    CREATETABLE(ERANDIXSITEMANAGERLINK,localStorage.getItem("Config"),()=>{
-
-        GETDATA(ERANDIXSITEMANAGERLINK,localStorage.getItem("Config"),(Data)=>{
-    
-            FINDER(Data,"ID",localStorage.getItem("VisitorID"),(User)=>{
-    
-                CONDITION(User === false,()=>{
-    
-                    DEVICE((Datata)=>{
-    
-                        const INFO=[Datata.userAgent,`[${new Date()}]`,1,Datata.platform,Datata.language,Datata.screen,Datata,"Approved"];
-                        
-                        INSERTDATA(ERANDIXSITEMANAGERLINK,localStorage.getItem("Config"),HEADERS,INFO,(DataBack)=>{
-    
-                            DATASTORE(" ","VisitorID",DataBack.uniqueId);
-    
-                        });
-    
-                    });
-    
-                },()=>{
-    
-                    DEVICE((Datata)=>{
-    
-                        JSONADDER(User.DeviceTime,[new Date()],(Info)=>{
-    
-                            const INFO=[Datata.userAgent,Info,User.VisitedTimes+1,Datata.platform,Datata.language,Datata.screen,Datata,"Approved"];
-                            
-                            UPDATEDATA(ERANDIXSITEMANAGERLINK,localStorage.getItem("Config"),localStorage.getItem("VisitorID"),INFO,(DataBack)=>{
+        console.log("Dev");
         
-                                DATASTORE(" ","VisitorID",localStorage.getItem("VisitorID"));
+    },()=>{
+
+        const HEADERS=["DeviceName","DeviceTime","VisitedTimes","Platform","Language","Screen","FullData","Approved"];
+    
+        CREATETABLE(ERANDIXSITEMANAGERLINK,localStorage.getItem("Config"),()=>{
+    
+            GETDATA(ERANDIXSITEMANAGERLINK,localStorage.getItem("Config"),(Data)=>{
+        
+                FINDER(Data,"ID",localStorage.getItem("VisitorID"),(User)=>{
+        
+                    CONDITION(User === false,()=>{
+        
+                        DEVICE((Datata)=>{
+        
+                            const INFO=[Datata.userAgent,`[${new Date()}]`,1,Datata.platform,Datata.language,Datata.screen,Datata,"Approved"];
+                            
+                            INSERTDATA(ERANDIXSITEMANAGERLINK,localStorage.getItem("Config"),HEADERS,INFO,(DataBack)=>{
+        
+                                DATASTORE(" ","VisitorID",DataBack.uniqueId);
         
                             });
-    
+        
                         });
-    
+        
+                    },()=>{
+        
+                        DEVICE((Datata)=>{
+        
+                            JSONADDER(User.DeviceTime,[new Date()],(Info)=>{
+        
+                                const INFO=[Datata.userAgent,Info,User.VisitedTimes+1,Datata.platform,Datata.language,Datata.screen,Datata,"Approved"];
+                                
+                                UPDATEDATA(ERANDIXSITEMANAGERLINK,localStorage.getItem("Config"),localStorage.getItem("VisitorID"),INFO,(DataBack)=>{
+            
+                                    DATASTORE(" ","VisitorID",localStorage.getItem("VisitorID"));
+            
+                                });
+        
+                            });
+        
+                        });
+        
                     });
-    
+        
                 });
-    
+        
             });
     
         });
 
     });
-
+    
 };
