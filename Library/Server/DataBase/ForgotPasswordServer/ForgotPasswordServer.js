bfffe993ad41854f9a4f18d4d5772cfb4da28ba2
Elite Robust Ontology <erandix9@gmail.com>
2026-02-05 21:15:58 +0300

    Updates

--- Library/Server/DataBase/ForgotPasswordServer/ForgotPasswordServer.js
+++ Library/Server/DataBase/ForgotPasswordServer/ForgotPasswordServer.js
@@ -8,7 +8,7 @@ export const FORGOTPASSWORDSERVER=(LINK)=>{
 
             CONDITION(User.UserEmail === sessionStorage.getItem("UserEmail"),()=>{
                 
-                const MESSAGE=`Dear ${User.UserEmail};\n Your Account Password Is Below\n ===>${User.UserPasssword}.\n If Your The One Who Tried to Access Your Account Please Open Respective Point and Don't Share Your Password,\n\n If your Not the one Who Requested for Account to be Opened,Please Ignore This Mail.\n Thank You.`
+                const MESSAGE=`Dear ${User.UserName};\n Your Account Password Is Below\n ===>${User.UserPassword}.\n If Your The One Who Tried to Access Your Account Please Open Respective Point and Don't Share Your Password,\n\n If your Not the one Who Requested for Account to be Opened,Please Ignore This Mail.\n Thank You.`
     
                 EMAILSERVER(sessionStorage.getItem("UserEmail"),"Account Creation",MESSAGE,()=>{
 
