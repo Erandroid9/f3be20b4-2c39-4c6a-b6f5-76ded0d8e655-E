5f5b95cf04ac95f6f4f78fbbe8dbb9bb3e6d4247
Elite Robust Ontology <erandix9@gmail.com>
2026-02-11 13:26:17 +0300

    Updates

--- Library/Functions/DataBase/ServerComponents/ServerComponents.js
+++ Library/Functions/DataBase/ServerComponents/ServerComponents.js
@@ -6,7 +6,7 @@ export const SERVERCOMPONENTS=(PATH,CALLBACK)=>{
 
     },()=>{
 
-        CALLBACK("https://erandroid9.github.io/Eliton/Library/Assets/DataBase/Raw/"+PATH);
+        CALLBACK("https://erandroid9.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-E/Library/Assets/DataBase/Raw/"+PATH);
 
     });
 
