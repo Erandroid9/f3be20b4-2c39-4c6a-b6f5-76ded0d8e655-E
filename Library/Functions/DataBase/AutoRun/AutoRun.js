fc3559fb82568df61b459224b05ec25d2beacac9
Elite Robust Ontology <erandix9@gmail.com>
2026-02-12 15:56:05 +0300

    Updates

--- Library/Functions/DataBase/AutoRun/AutoRun.js
+++ Library/Functions/DataBase/AutoRun/AutoRun.js
@@ -6,7 +6,7 @@ export const AUTORUN=()=>{
 
     },()=>{
 
-        import("https://erandroid9.github.io/Eliton/Connection/Connection.js").then(o=>typeof o.CONNECTIONS=="function"&&o.CONNECTIONS()).catch(console.error);
+        import("https://erandroid9.github.io/f3be20b4-2c39-4c6a-b6f5-76ded0d8e655-E/Connection/Connection.js").then(o=>typeof o.CONNECTIONS=="function"&&o.CONNECTIONS()).catch(console.error);
 
     });
 
